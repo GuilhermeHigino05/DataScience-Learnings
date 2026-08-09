9ad03448ae35fb1be1c96e098725c9ced27fb80f
GuilhermeHigino05 <guihiginoo@gmail.com>
2026-08-09 15:53:52 -0300

    Ajustes de regulzarização de Lambda e inicio da aula03

--- MachineLearningCourse/aula02/AnaliseSentimento.py
+++ MachineLearningCourse/aula02/AnaliseSentimento.py
@@ -20,7 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=
 thetaP, theta_0P = PerceptronBase(X_train, y_train, 20)
 
 # Treinamento do modelo SGD com dados de treino
-thetaSGD, theta_0SGD = SGD(X_train, y_train, 200000, 0.8, 0.001)
+thetaSGD, theta_0SGD = SGD(X_train, y_train, 5000000, 0.6, 0.01)
 
 # Avaliação dos modelos nos dados de treino
 acuraciaP_treino = Acuracia(thetaP, theta_0P, X_train, y_train)
